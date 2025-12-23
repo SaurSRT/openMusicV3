67d78142094a13ae8de4ebeac920a92685d95e2b
SaurSRT <ferdinand.saur03@gmail.com>
2025-12-24 01:09:40 +0700

    init openmusic v3

---
+++ migrations/1766234307483_playlists.js
@@ -0,0 +1,16 @@
+exports.up = (pgm) => {
+  pgm.createTable('playlists', {
+    id: { type: 'VARCHAR(50)', primaryKey: true },
+    name: { type: 'TEXT', notNull: true },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlists');
+};
